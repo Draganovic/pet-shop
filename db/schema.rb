121224ddcdb38145e39224ac1bba6ff461f205bf
Admir Draganovic <admir.draganovic@gmail.com>
2016-04-01 13:37:10 -0700

    Added price to the puppies.
    Migration complete for puppy price.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330214200) do
+ActiveRecord::Schema.define(version: 20160401011430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160330214200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "shop_id"
+    t.integer  "price"
   end
 
   add_index "puppies", ["shop_id"], name: "index_puppies_on_shop_id", using: :btree
